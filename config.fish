d4224909968460759fd66da32c6b78363ec66191
Jim Dibb <jimdibb@gmail.com>
2019-01-16 07:25:21 -0500

    more minimal theme for win

--- config.fish
+++ config.fish
@@ -1,23 +1,19 @@
-set normal (set_color normal)
-set magenta (set_color magenta)
-set yellow (set_color yellow)
-set green (set_color green)
-set red (set_color red)
-set gray (set_color -o black)
+cd ~
+# eval (keychain -c --eval id_rsa)
+function ssh_agent --description 'launch the ssh-agent and add the id_rsa identity'
+    if begin
+            set -q SSH_AGENT_PID
+            and kill -0 $SSH_AGENT_PID
+            and grep -q '^ssh-agent' /proc/$SSH_AGENT_PID/cmdline
+    end
+        echo "ssh-agent running on pid $SSH_AGENT_PID"
+    else
+        eval (command ssh-agent -c | sed 's/^setenv/set -Ux/')
+    end
+    set -l identity $HOME/.ssh/id_rsa
+    set -l fingerprint (ssh-keygen -lf $identity | awk '{print $2}')
+    ssh-add -l | grep -q $fingerprint
+        or ssh-add $identity
+end
 
-# Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
-
-# Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
+ssh_agent
